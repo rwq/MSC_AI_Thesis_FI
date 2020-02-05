b4d283274dab5e9efe66d88a152b330567195fd3
rwq <rlwijma@gmail.com>
2020-02-05 11:45:16 +0100

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -15,7 +15,7 @@ def psnr(y_hat, y):
     R = max(y.max(), y_hat.max()).float()
     batch_mse = (y_hat-y).pow(2).float().mean(dim=(1,2,3))
     
-    batch_psnr = 20 * torch.log10(R**2) - 10 * torch.log10(batch_mse)
+    batch_psnr = 20 * torch.log10(R) - 10 * torch.log10(batch_mse)
     
     return batch_psnr
 
@@ -27,8 +27,10 @@ def interpolation_error(y_hat, y):
     batch_size x n_channels x height x width
     '''
     
-    assert y_hat.size() == y.size(), 'Input dimensions must match.'
+    assert y_hat.size() == y.size(), \
+        f'Input dimensions must match.  DIM1: {y_hat.size()}, DIM2: {y.size()}'
     
+
     mse = (y_hat-y).pow(2).mean(dim=(1,2,3))
     
     return mse.sqrt()
@@ -41,7 +43,9 @@ def ssim(im1, im2, c1=1., c2=1., c3=1.):
     batch_size x n_channels x height x width
     '''
     
-    assert im1.size() == im2.size(), 'Input dimensions must match.'
+    assert im1.size() == im2.size(),  \
+        f'Input dimensions must match.  DIM1: {im1.size()}, DIM2: {im2.size()}'
+    
     
     im1 = im1.float()
     im2 = im2.float()
@@ -52,10 +56,9 @@ def ssim(im1, im2, c1=1., c2=1., c3=1.):
     sig1 = im1.std(dim=(1,2,3), keepdims=True)
     sig2 = im2.std(dim=(1,2,3), keepdims=True)
     
-    
     # calculate covariance
-    e1 = (im1-mu1).view(im1.size(0), -1)
-    e2 = (im2-mu2).view(im2.size(0), -1)
+    e1 = (im1-mu1).reshape(im1.size(0), -1)
+    e2 = (im2-mu2).reshape(im2.size(0), -1)
     cov = (e1 * e2).sum(dim=1) / (e1.size(1)-1)
     
     # remove dimensions
@@ -63,9 +66,6 @@ def ssim(im1, im2, c1=1., c2=1., c3=1.):
     mu2 = mu2.squeeze()
     sig1 = sig1.squeeze()
     sig2 = sig2.squeeze()
-    
-    print(mu1.shape, mu2.shape, e1.shape, e2.shape)
-    print(sig1.shape, sig2.shape, cov.shape)
     
     # compute ssim
     L = (2 * mu1 * mu2 + c1) / (mu1**2 + mu2**2 + c1)
