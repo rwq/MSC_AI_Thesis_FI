c7e3b92cabee94c76e67ca7942c1f68044f6e9d7
rwq <rlwijma@gmail.com>
2020-06-29 12:10:38 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from pytorch_msssim import ssim as mssim
 
 def psnr(y_hat, y, R=255.0):
     '''
@@ -16,7 +17,8 @@ def psnr(y_hat, y, R=255.0):
     # R = max(y.max(), y_hat.max()).float()
     batch_mse = (y_hat-y).pow(2).float().mean(dim=(1,2,3))
     
-    batch_psnr = 20 * np.log10(R) - 10 * torch.log10(batch_mse)
+    # clamp to prevent log 0
+    batch_psnr = 20 * np.log10(R) - 10 * torch.log10(batch_mse.clamp(1e-10))
     
     return batch_psnr.tolist()
 
@@ -37,40 +39,24 @@ def interpolation_error(y_hat, y):
     return mse.sqrt().tolist()
 
 
-def ssim(im1, im2, c1=1., c2=1., c3=1.):
+
+def std(tensor, dim, keepdim):
+    device = tensor.device
+    numpy_result = tensor.detach().cpu().numpy().std(axis=dim, keepdims=keepdim, ddof=1)
+    return torch.from_numpy(numpy_result).to(device)
+
+
+
+def ssim(im1, im2):
     '''
     Computes the SSIM for color images.
     Both inputs should be of shape:
     batch_size x n_channels x height x width
+
+    https://github.com/VainF/pytorch-msssim
     '''
     
-    assert im1.size() == im2.size(),  \
-        f'Input dimensions must match.  DIM1: {im1.size()}, DIM2: {im2.size()}'
+    return mssim(im1, im2, win_size=11, data_range=255, size_average=False).tolist()
     
     
-    im1 = im1.float()
-    im2 = im2.float()
     
-    # compute image statistics
-    mu1 = im1.mean(dim=(1,2,3), keepdims=True)
-    mu2 = im2.mean(dim=(1,2,3), keepdims=True)
-    sig1 = im1.std(dim=(1,2,3), keepdims=True)
-    sig2 = im2.std(dim=(1,2,3), keepdims=True)
-    
-    # calculate covariance
-    e1 = (im1-mu1).reshape(im1.size(0), -1)
-    e2 = (im2-mu2).reshape(im2.size(0), -1)
-    cov = (e1 * e2).sum(dim=1) / (e1.size(1)-1)
-    
-    # remove dimensions
-    mu1 = mu1.squeeze()
-    mu2 = mu2.squeeze()
-    sig1 = sig1.squeeze()
-    sig2 = sig2.squeeze()
-    
-    # compute ssim
-    L = (2 * mu1 * mu2 + c1) / (mu1**2 + mu2**2 + c1)
-    C = (2 * sig1 * sig2 + c2) / (sig1**2 + sig2**2 + c2)
-    S = (cov + c3) / (sig1 * sig2 + c3)
-    
-    return (L * C * S).tolist()
